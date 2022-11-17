14e8c32e5ea57adc5d9482f00ce81dba97cb4e36
Jason Chen <105614698+jnchen12a@users.noreply.github.com>
2022-11-17 17:54:41 +0000

    Started draw_start_screen function.

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 import pygame
 from constants import *
+from board import Board
+from cell import Cell
+from sudoku_generator import SudokuGenerator
+
+def draw_start_screen(screen):
+  screen.fill(WHITE)
+
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+
+def draw_difficulty_screen(screen):
+  pass
+
+def draw_game_screen(screen):
+  pass
+
+def draw_end_screen(screen):
+  pass
 
 if __name__ == '__main__':
   pygame.init()
   pygame.display.set_caption('Sudoku') # Title of the window
-  screen = pygame.display.set_mode((600, 600)) # Size of the display
+  screen = pygame.display.set_mode((600, 1000)) # Size of the display
+  screen.fill(WHITE)
   
   while True: # Event loop
     for event in pygame.event.get():
